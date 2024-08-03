48c48128b711c2f52164ba859ba0f0e2713cb38e
TomasUesato <uesatotomas@gmail.com>
2024-08-03 09:28:41 -0300

    week 1 ex 03

---
+++ CS61A/week01/ex03.scm
@@ -0,0 +1,21 @@
+#lang simply-scheme
+
+(define (replace word index)
+  (cond ((member? word '(I me)) 'you)
+        ((member? word '(You you))
+         (if (= index 0) 'I 'me))
+        (else word)))
+
+
+(define (switch snt)
+  (define (switch-helper snt index result)
+  (if (null? snt)
+      result
+      (switch-helper (bf snt) (+ 1 index) (sentence result (replace (first snt) index)))))
+  (switch-helper snt 0 '()))
+
+(define (member? word snt)
+  (cond ((null? snt) #false)
+        ((equal? word (first snt)) #true)
+        (else (member? word (bf snt)))))
+
