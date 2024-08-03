b5baaf787e81bf560a58eeb9a9d1833cfc78f5e8
TomasUesato <uesatotomas@gmail.com>
2024-08-03 10:41:36 -0300

    formatting

--- CS61A/week01/ex04.scm
+++ CS61A/week01/ex04.scm
@@ -5,7 +5,7 @@
   (define (ordered-helper numbers prev)
     (cond ((null? numbers) #true)
           ((> prev (first numbers)) #false)
-          (else (ordered-helper  (bf numbers) (first numbers)))))
+          (else (ordered-helper (bf numbers) (first numbers)))))
   (if (null? numbers)
       #true
       (ordered-helper (bf numbers) (first numbers))))
