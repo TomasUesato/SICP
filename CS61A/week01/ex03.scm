fc322c6c263c6b85d27b1ca60e685675762a28bb
TomasUesato <uesatotomas@gmail.com>
2024-08-03 09:36:41 -0300

    added block structure

--- CS61A/week01/ex03.scm
+++ CS61A/week01/ex03.scm
@@ -1,14 +1,12 @@
 #lang simply-scheme
 
-(define (replace word index)
-  (cond ((member? word '(I me)) 'you)
-        ((member? word '(You you))
-         (if (= index 0) 'I 'me))
-        (else word)))
-
-
 (define (switch snt)
   (define (switch-helper snt index result)
+    (define (replace word index)
+      (cond ((member? word '(I me)) 'you)
+            ((member? word '(You you))
+             (if (= index 0) 'I 'me))
+            (else word)))
   (if (null? snt)
       result
       (switch-helper (bf snt) (+ 1 index) (sentence result (replace (first snt) index)))))
