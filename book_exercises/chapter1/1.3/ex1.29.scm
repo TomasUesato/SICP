0a82207658db9a2eff8bda1981e09fdbef75a7dc
TomasUesato <uesatotomas@gmail.com>
2024-08-07 08:43:26 -0300

    book exercise 1.3.29

---
+++ book_exercises/chapter1/1.3/ex1.29.scm
@@ -0,0 +1,33 @@
+#lang simply-scheme
+
+(define (sum a b term next)
+  (if (> a b)
+      0
+      (+ (term a) (sum (next a) b term next))))
+
+(define (inc n)
+  (+ n 1))
+
+(define (cube x)
+  (* x x x))
+
+(define (simpson-coefficient x n)
+  (cond ((or (= x 0) (= x n)) 1.0)
+        ((even? x) 2.0)
+        (else 4.0 )))
+
+(define (simpson-term f a k h)
+  (f (+ a (* k h))))
+
+
+(define (simpsons-integral f a b n)
+  (let ((h (/ (- b a) n)))
+  (define (combined-term k)
+    (* (simpson-coefficient k n) (simpson-term f a k h)))
+  (* (/ h 3.0) (sum 0 n combined-term inc))))
+
+
+  
+
+        
+    
