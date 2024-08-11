eac9df5a8471e816dfb2aef17bcef467de6e4c01
TomasUesato <uesatotomas@gmail.com>
2024-08-10 22:04:42 -0300

    book exercise 1.32

---
+++ book_exercises/chapter1/1.3/ex1.32.scm
@@ -0,0 +1,23 @@
+#lang simply-scheme
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate-iterative combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner (term a) result))))
+  (iter a null-value))
+
+(define (sum a b term next)
+  (accumulate + 0 term a next b))
+
+(define (sum-integers a b)
+  (sum a b identity inc))
+
+(define (identity x) x)
+
+(define (inc x) (+ x 1))
