52318b6eac320f5883ccc3a6a11357fc67daedd6
TomasUesato <uesatotomas@gmail.com>
2024-08-13 21:25:35 -0300

    book exercise 1.33, part a

---
+++ book_exercises/chapter1/1.3/ex1.33.scm
@@ -0,0 +1,36 @@
+#lang simply-scheme
+
+(define (filtered-accumulate combiner null-value term a next b filter)
+  (cond ((> a b) null-value)
+        ((filter a) (combiner (term a) (filtered-accumulate combiner null-value term (next a) next b filter)))
+        (else (filtered-accumulate combiner null-value term (next a) next b filter))))
+
+(define (sum-squares-of-primes a b)
+  (sum a b square inc prime?))
+
+(define (sum a b term next filter)
+  (filtered-accumulate + 0 term a next b filter))
+
+(define (prime? x)
+  (if (= x 1)
+      #f
+      (= (smallest-divisor x) x)))
+
+(define (smallest-divisor x)
+  (cond  ((even? x) 2)
+         (else (find-divisor-from x 3))))
+
+(define (find-divisor-from n k)
+  (cond ((> (square k) n) n)
+        ((divides? k n) k)
+        (else (find-divisor-from n (+ k 2)))))
+
+(define (divides? a b)
+  (= (modulo b a) 0))
+
+
+(define (square x) (* x x))
+(define (identity x) x)
+(define (inc x) (+ x 1))
+
+                              
